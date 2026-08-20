90f8841ce1ebe4649ac691fad4b0c301bbb8eb5a
Jorink <maxjorink@gmail.com>
2026-08-21 00:03:39 +0200

    Fix crashing on quest standalone, the game would crash if you turned the mod off, so I removed the toggle after trying to fix it

--- docs/README.md
+++ docs/README.md
@@ -1,6 +1,11 @@
 # NoVirtualCrouch
-A BoneLab mod that allows you to toggle on/off virtual crouching input. <br>
+A BoneLab mod that disables virtual crouching input. <br>
 Last tested on `Patch 6` <br>
+
+## Settings
+There are not settings right now, the mod is permanently on (untill you delete it.) <br>
+The reason for this is because the game would crash when I try implementing a toggle for it, I might try again in the future. <br>
+But for now: You cannot disable the mod unless you delete it! <br>
 
 ## Discord Server
 Join the Discord server to chat, give suggestions, report issues, and get sneak peeks! <br>
--- src/NoVirtualCrouch.cs
+++ src/NoVirtualCrouch.cs
@@ -1,7 +1,6 @@
 using HarmonyLib;
 using MelonLoader;
 using BoneLib;
-using BoneLib.BoneMenu;
 using Il2CppSLZ.Marrow;
 using UnityEngine;
 
@@ -12,39 +11,14 @@ namespace NoVirtualCrouch
 {
     public class Core : MelonMod
     {
-        private static MelonPreferences_Category category;
-        private static MelonPreferences_Entry<bool> crouchEntry;
-
         private static BaseController GetController() => Player.RightController;
-
-        public override void OnInitializeMelon()
-        {
-            SetupMelonPreferences();
-            SetupBoneMenu();
-        }
-
-        private void SetupBoneMenu()
-        {
-            Page defaultPage = Page.Root.CreatePage("Jorink", Color.red).CreatePage("NoVirtualCrouch", Color.yellow);
-
-            defaultPage.CreateBool("Virtual Crouch", Color.yellow, crouchEntry.Value, (value) => crouchEntry.Value = value);
-            defaultPage.CreateFunction("Save Settings", Color.cyan, () => MelonPreferences.Save());
-        }
-
-        private void SetupMelonPreferences()
-        {
-            category = MelonPreferences.CreateCategory("NoVirtualCrouch");
-            crouchEntry = category.CreateEntry("Virtual Crouch", true);
-            MelonPreferences.Save();
-            category.SaveToFile();
-        }
 
         [HarmonyPatch(typeof(OpenController), nameof(OpenController.GetThumbStickAxis))]
         private static class SuppressVirtualCrouch
         {
             private static void Postfix(OpenController __instance, ref Vector2 __result)
             {
-                if (!crouchEntry.Value && __instance == GetController())
+                if (__instance == GetController())
                 {
                     __result.y = 0f;
                 }
