7a2b07de027b52f905c80c86c3d57057f6659ade
Jorink <maxjorink@gmail.com>
2026-09-01 22:56:49 +0200

    Fix AssemblyInfo colors

--- Mod/Properties/AssemblyInfo.cs
+++ Mod/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using novirtualcrouch;
 
 [assembly: MelonInfo(typeof(NoVirtualCrouch), "NoVirtualCrouch", NoVirtualCrouch.Version, "Jorink")]
 [assembly: MelonGame("Stress Level Zero", "BONELAB")]
-[assembly: MelonColor(0,0,255,1)]
-[assembly: MelonAuthorColor(255,0,0,1)]
+[assembly: MelonColor(255,0,0,255)]
+[assembly: MelonAuthorColor(255,255,0,0)]
 [assembly: MelonPriority(1)]
